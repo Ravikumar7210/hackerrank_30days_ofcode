ea72bd46770c29bf8ef5660a2d369a59bf6abcfc
Ravi Kumar <Rk76912@gmail.com>
2021-07-10 19:54:08 +0530

    day3 code uploded

--- day2.cpp
+++ day2.cpp
@@ -2,6 +2,9 @@
 
 using namespace std;
 
+string ltrim(const string &);
+string rtrim(const string &);
+
 /*
  * Complete the 'solve' function below.
  *
@@ -38,5 +41,27 @@ int main()
     solve(meal_cost, tip_percent, tax_percent);
 
     return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
 }
 
